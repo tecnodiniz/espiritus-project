583a5590f2be975b51b39e5d2422611290d325da
Eduardo Diniz <diniz480@gmail.com>
2025-02-21 14:01:40 -0300

    card created on home

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,5 +1,31 @@
-const Home = () => {
-  return <h1>Home Page</h1>;
-};
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
-export default Home;
+export default function Home() {
+  return (
+    <Card className="w-[350px]">
+      <div className="flex items-center">
+        <Avatar className="size-15 ml-3">
+          <AvatarImage src="https://thispersondoesnotexist.com/" />
+          <AvatarFallback>Avatar</AvatarFallback>
+        </Avatar>
+        <CardHeader className="px-2">
+          <CardTitle>Eduardo Diniz</CardTitle>
+          <CardDescription>Usuário no plano basico</CardDescription>
+        </CardHeader>
+      </div>
+
+      <CardFooter className="flex justify-end">
+        <Button>Mais</Button>
+      </CardFooter>
+    </Card>
+  );
+}
