dc842d96ed833dcbbd6d3154cdfb37835016e24a
Eduardo Diniz <diniz480@gmail.com>
2025-02-22 21:15:06 -0300

    Search engine done

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -9,6 +9,7 @@ import {
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { use, useEffect, useState } from "react";
 import { fetch_users } from "@/services/api";
+import { Input } from "@/components/ui/input";
 
 type User = {
   name: string;
@@ -17,6 +18,8 @@ type User = {
 
 export default function Home() {
   const [users, setUsers] = useState<User[]>([]);
+  const [filteredUsers, setFilteredUsers] = useState<User[]>([]);
+  const [query, setQuery] = useState("");
 
   async function fetchUsers() {
     try {
@@ -42,30 +45,50 @@ export default function Home() {
   }, []);
 
   useEffect(() => {
-    console.log(users);
-  }, [users]);
-  return (
-    <div className="flex flex-wrap gap-4 justify-center">
-      {users.map((user, index) => (
-        <Card className="w-[350px] " key={index}>
-          <div className="flex items-center">
-            <Avatar className="size-15 ml-3">
-              <AvatarImage src="" />
-              <AvatarFallback>{getInitials(user.name)}</AvatarFallback>
-            </Avatar>
-            <CardHeader className="px-2">
-              <CardTitle>{user.name}</CardTitle>
-              <CardDescription>Usuário no plano {user.plan}</CardDescription>
-            </CardHeader>
-          </div>
+    if (!query.trim()) {
+      setFilteredUsers(users);
+    } else
+      setFilteredUsers(
+        users.filter((user) =>
+          user.name.toLowerCase().includes(query.toLocaleLowerCase())
+        )
+      );
+  }, [query]);
 
-          <CardFooter className="flex justify-end">
-            <Button className="cursor-pointer" variant="link">
-              Mais
-            </Button>
-          </CardFooter>
-        </Card>
-      ))}
+  useEffect(() => {
+    setFilteredUsers(users);
+  }, [users]);
+
+  return (
+    <div className="grid grid-cols-1 space-y-6">
+      <Input
+        type="text"
+        placeholder="Search Users"
+        onChange={(e) => setQuery(e.target.value)}
+      />
+
+      <div className="flex flex-wrap gap-4 justify-center">
+        {filteredUsers.map((user, index) => (
+          <Card className="w-[350px] " key={index}>
+            <div className="flex items-center">
+              <Avatar className="size-15 ml-3">
+                <AvatarImage src="" />
+                <AvatarFallback>{getInitials(user.name)}</AvatarFallback>
+              </Avatar>
+              <CardHeader className="px-2">
+                <CardTitle>{user.name}</CardTitle>
+                <CardDescription>Usuário no plano {user.plan}</CardDescription>
+              </CardHeader>
+            </div>
+
+            <CardFooter className="flex justify-end">
+              <Button className="cursor-pointer" variant="link">
+                Mais
+              </Button>
+            </CardFooter>
+          </Card>
+        ))}
+      </div>
     </div>
   );
 }
