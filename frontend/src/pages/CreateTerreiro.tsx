f1eebada885de2815126b238e3ec83ec54a8944e
Eduardo Diniz <diniz480@gmail.com>
2025-03-11 12:42:46 -0300

    WIP terreiroForm

--- frontend/src/pages/CreateTerreiro.tsx
+++ frontend/src/pages/CreateTerreiro.tsx
@@ -10,7 +10,15 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { useProfile } from "@/context/ProfileContext";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 
@@ -23,9 +31,13 @@ const formSchema = z.object({
     .regex(new RegExp("^(?!\\s)(?=.*[a-zA-Z])[a-zA-Z0-9 \\s]+$"), {
       message: "Nome inválido",
     }),
+  segment: z.string().nonempty({
+    message: "Selecione um segmento",
+  }),
 });
 
 export function CreateTerreiro() {
+  const { profile } = useProfile();
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -34,36 +46,85 @@ export function CreateTerreiro() {
   });
 
   function onSubmit(values: z.infer<typeof formSchema>) {
-    console.log(values);
+    const payload = { ...values, leader: profile?.id || 0 };
+    console.log(payload);
   }
   return (
     <div className="container max-w-[1280px] md:w-[800px] mx-auto">
-      <Card>
+      <Card className="px-4">
         <CardHeader>
           <CardTitle>
-            <p className="text-3xl text-purple-900">Cadastro de Terreiro</p>
+            <p className="text-3xl text-purple-900 font-semibold">
+              Cadastro de Terreiro
+            </p>
           </CardTitle>
         </CardHeader>
         <CardContent>
           <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-              <FormField
-                control={form.control}
-                name="name"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Nome do Terreiro</FormLabel>
-                    <FormControl>
-                      <Input placeholder="Nome do Terreiro" {...field} />
-                    </FormControl>
-                    <FormDescription>
-                      Nome que aparecerá para os usuários
-                    </FormDescription>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <Button type="submit">Adicionar</Button>
+            <form
+              onSubmit={form.handleSubmit(onSubmit)}
+              className="grid grid-cols-2 gap-6 space-y-4 items-center"
+            >
+              <div className="col-span-2">
+                <FormField
+                  control={form.control}
+                  name="name"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel className="text-xl font-normal">
+                        Nome do Terreiro
+                      </FormLabel>
+                      <FormControl>
+                        <Input
+                          placeholder="Nome do Terreiro"
+                          className="mt-2"
+                          {...field}
+                        />
+                      </FormControl>
+
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+
+              <div className="w-full">
+                <FormField
+                  control={form.control}
+                  name="segment"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel className="text-xl font-normal">
+                        Segmento
+                      </FormLabel>
+                      <Select
+                        onValueChange={field.onChange}
+                        defaultValue={field.value}
+                      >
+                        <FormControl>
+                          <SelectTrigger className="w-full mt-2">
+                            <SelectValue placeholder="Selecione um segmento" />
+                          </SelectTrigger>
+                        </FormControl>
+                        <SelectContent>
+                          <SelectItem value="Umbanda">Umbanda</SelectItem>
+                          <SelectItem value="Candomlé">Candomblé</SelectItem>
+                        </SelectContent>
+                      </Select>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+
+              <div className="col-span-2 flex gap-4 justify-end">
+                <Button variant="outline" className="cursor-pointer">
+                  Cancelar
+                </Button>
+                <Button type="submit" className="cursor-pointer">
+                  Adicionar
+                </Button>
+              </div>
             </form>
           </Form>
         </CardContent>
